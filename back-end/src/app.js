83a4c0d54736e815ddb768e83549a3923949c441
Samuel Guzman <creativebusinez@gmail.com>
2023-12-07 21:03:48 -0500

    installed and tested react to make sure it was working

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -56,7 +56,7 @@ app.use((error, req, res, next) => {
 
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });

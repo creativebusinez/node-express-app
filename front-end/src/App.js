26b5d495ca6be93e9071293a71345645d9b57509
Samuel Guzman <creativebusinez@gmail.com>
2023-12-09 19:13:57 -0500

    modified front-end directory

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,61 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/layout/Header'; // Assuming you have a Header component
+import Footer from './components/layout/Footer'; // Assuming you have a Footer component
+import Home from './pages/Home'; // Home page component
+import Blog from './pages/Blog'; // Blog page component
+import About from './pages/About'; // About page component
+import Contact from './pages/Contact'; // Contact page component
+import './App.css'; // Main stylesheet (modify as needed)
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to My React Blogging Platform</h1>
-      <p>This is a test to ensure React is working correctly.</p>
-    </div>
+    <Router>
+      <div className="App">
+        <Header /> {/* Site header */}
+        <main>
+          <Switch>
+            {/* Define routes for different pages */}
+            <Route path="/" exact component={Home} />
+            <Route path="/blog" component={Blog} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            {/* You can add more routes here as needed */}
+          </Switch>
+        </main>
+        <Footer /> {/* Site footer */}
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>Welcome to My React Blogging Platform</h1>
+//       <p>This is a test to ensure React is working correctly.</p>
+//     </div>
+//   );
+// }
+
+// export default App;
 
 
 
